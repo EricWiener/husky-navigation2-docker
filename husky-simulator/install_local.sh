2fa2ea6620b292841b6f1a6bf9fe472aa96f2f92
Eric Wiener <ericwiener3@gmail.com>
2021-02-05 15:13:10 -0500

    FEAT: updated install_local.sh to setup ROS as well

--- husky-simulator/install_local.sh
+++ husky-simulator/install_local.sh
@@ -3,12 +3,20 @@
 # sudo ./install_local.sh
 
 export rosversion='melodic'
+
+# Setup sources.list
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+# Setup keys
+sudo apt-key adv --kesyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+
 sudo apt-get update
-sudo apt-get update && apt-get install -y python3-pip \
+sudo apt-get update && sudo apt-get install -y python3-pip \
      vim wget iputils-ping \
-    "ros-${rosversion}-husky-desktop" \
-    "ros-${rosversion}-husky-simulator" \
-    "ros-${rosversion}-navigation" \
-    "ros-${rosversion}-husky-gazebo" \
-    "ros-${rosversion}-husky-viz" \
-    "ros-${rosversion}-husky-navigation"
+     "ros-${rosversion}-desktop-full" \
+     "ros-${rosversion}-husky-desktop" \
+     "ros-${rosversion}-husky-simulator" \
+     "ros-${rosversion}-navigation" \
+     "ros-${rosversion}-husky-gazebo" \
+     "ros-${rosversion}-husky-viz" \
+     "ros-${rosversion}-husky-navigation"
